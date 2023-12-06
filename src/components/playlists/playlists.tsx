6f179667e208f15b7dbeab9d3d4a3f3c61d75fed
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-12-06 14:49:07 -0600

    fixed playlist list not using grids on small screens

--- src/components/playlists/playlists.tsx
+++ src/components/playlists/playlists.tsx
@@ -7,7 +7,7 @@ export const Playlists = (props: {
 }) => {
   return (
     <div className="flex flex-col w-full">
-      <div className="grid gap-2 md:grid-cols-3 lg:grid-cols-4 w-full">
+      <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
         {props.state.items.map((item) => {
           return <PlaylistPreview playlist={item} key={item.id} />;
         })}
