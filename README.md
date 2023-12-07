5d956a9ed10bf89e47a946a304ab6d50a810ef69
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-12-07 07:29:25 -0600

    added docs

--- README.md
+++ README.md
@@ -1,36 +1,28 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Spotify Playlist Analyzer
 
-## Getting Started
+## Status: 🚧 Under Construction 🚧
+* View the latest version at [https://pwr.jfrazier.dev](https://pwr.jfrazier.dev)
 
-First, run the development server:
+## Features
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+### View playlists
+![home page]("")
+* Select a playlist to view more info.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+### View playlist details
+![playlist page]("")
+* View playlist average valence, energy, and danceability.
+* Utilize the line chart view trends across songs in a playlist.
+* View the list of tracks along with the song's general stats.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+## Goals and Planned Features
 
-## Learn More
+### Goals
+* Create my ideal playlist builder. I'll be utilizing this to create my playlists and will prioritize features that improve my productivity and quality.
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### Planned Features
+* Integrate a player - the ability to play songs/playlists/albums within the site.
+* Sort songs within a playlist by a specific metric.
+* Enable/disable which metrics are shown.
+* Metric first playlists - the user should be able to set the metrics they want and the genre, then we should utilize the recommendations endpoint to get songs for the playlist.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,39 @@ import { usePlaylists } from "@/components/hooks/playlists";
 import { useSpotify } from "@/components/hooks/useSpotify";
 import { useUser } from "@/components/hooks/user";
 import { Playlists } from "@/components/playlists/playlists";
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import { useSearchParams } from "next/navigation";
 
-export default function Home() {
-  const { sdk } = useSpotify();
+const LoggedOutUserHome = (props: { sdk: SpotifyApi }) => {
+  return (
+    <div className="overflow-scroll h-full w-full">
+      <header className="w-full py-12 text-gray-200">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col justify-center space-y-6 md:space-y-10 lg:space-y-14">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center">
+              Spotify Playlist Creator
+            </h1>
+            <p className="text-xl md:text-2xl lg:text-3xl text-gray-400 text-center">
+              Create your perfect Spotify playlists with advanced tooling
+            </p>
+            <button
+              className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 self-center px-8 py-3 rounded-md bg-green-500 text-white"
+              onClick={() => {
+                props.sdk.authenticate();
+              }}
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+const LoggedInUser = ({ sdk }: { sdk: SpotifyApi }) => {
   const user = useUser(sdk);
   const { state: playlistState } = usePlaylists(sdk);
-
   return (
     <div className="flex flex-col h-screen w-full">
       <div className="flex flex-row items-center mb-4">
@@ -44,4 +71,22 @@ export default function Home() {
       </div>
     </div>
   );
+};
+
+const checkIsLoggedIn = () => {
+  const value = localStorage.getItem(
+    "spotify-sdk:AuthorizationCodeWithPKCEStrategy:token",
+  );
+  return !!value;
+};
+export default function Home() {
+  const { sdk } = useSpotify();
+  const isLoggedIn = checkIsLoggedIn();
+  const search = useSearchParams();
+
+  if (!isLoggedIn && !search.get("code")) {
+    return <LoggedOutUserHome sdk={sdk} />;
+  } else {
+    return <LoggedInUser sdk={sdk} />;
+  }
 }
